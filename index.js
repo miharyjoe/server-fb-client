2d3bbf06d6b2c339e8e064d337c88769d19cd4cd
miharyjoel <miharyjoel@gmail.com>
2023-11-02 13:31:03 +0300

    add render  config

--- index.js
+++ index.js
@@ -44,5 +44,5 @@ app.post("/login", async (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running`);
+  console.log(`Server running on port ${process.env.PORT || 3000}`);
 });
