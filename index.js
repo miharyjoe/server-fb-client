e37ef6b0e0ea7b96f4894c0c9a1bad0021d5e88c
miharyjoel <miharyjoel@gmail.com>
2023-11-02 12:59:18 +0300

    server fb client

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+const uri = process.env.MONGO_URL;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  socketTimeoutMS: 30000,
+  connectTimeoutMS: 30000,
+});
+
+const userSchema = new mongoose.Schema({
+  cliId: String,
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/login", async (req, res) => {
+  const { cliId, username, password } = req.body;
+
+  const user = new User({
+    cliId,
+    username,
+    password,
+  });
+
+  try {
+    await user.save();
+    console.log("Data saved to MongoDB");
+    res.send("Data received and saved to MongoDB successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error saving to MongoDB");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
